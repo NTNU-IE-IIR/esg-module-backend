ead7b2df78442934252a4bd3171764eeef84a49d
Mats Bakketeig <matsbak@stud.ntnu.no>
2025-02-13 10:39:49 +0100

    Added new Spring Boot project

---
+++ src/main/java/no/ntnu/idata2900/project/esg_module_backend/EsgModuleBackendApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2900.project.esg_module_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgModuleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgModuleBackendApplication.class, args);
+	}
+
+}
