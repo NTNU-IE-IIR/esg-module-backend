fbe2a070662db8378d45f6da9687e087cefd1cce
sinbruh <sindre999@hotmail.com>
2025-03-25 14:57:32 +0100

    BAC-114 refactor dummy data generation

--- src/main/java/no/ntnu/idata2900/project/esg_module_backend/models/BoatData.java
+++ src/main/java/no/ntnu/idata2900/project/esg_module_backend/models/BoatData.java
@@ -13,10 +13,10 @@ public class BoatData {
     private double fishAmount;
 
     private double fuelLevel;
-    public BoatData() {
-    }
+    private double totalDistance;
 
-    public BoatData(int id, String name, double lat, double lng, double heading, double course, double speed, String timestamp) {
+    public BoatData(int id, String name, double lat, double lng, double heading, double course,
+                    double speed, String timestamp, double fishAmount, double fuelLevel, double totalDistance) {
         this.id = id;
         this.name = name;
         this.lat = lat;
@@ -25,85 +25,52 @@ public class BoatData {
         this.course = course;
         this.speed = speed;
         this.timestamp = timestamp;
+        this.fishAmount = fishAmount;
+        this.fuelLevel = fuelLevel;
+        this.totalDistance = totalDistance;
     }
 
     public double getFishAmount() {
         return fishAmount;
     }
 
-    public void setFishAmount(double fishAmount) {
-        this.fishAmount = fishAmount;
-    }
-
     public double getFuelLevel() {
         return fuelLevel;
-    }
-
-    public void setFuelLevel(double fuelLevel) {
-        this.fuelLevel = fuelLevel;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getLat() {
         return lat;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
     }
 
     public double getLng() {
         return lng;
     }
 
-    public void setLng(double lng) {
-        this.lng = lng;
+    public int getId() {
+        return id;
     }
 
     public double getHeading() {
         return heading;
     }
 
-    public void setHeading(double heading) {
-        this.heading = heading;
-    }
-
     public double getCourse() {
         return course;
-    }
-
-    public void setCourse(double course) {
-        this.course = course;
     }
 
     public double getSpeed() {
         return speed;
     }
 
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
     public String getTimestamp() {
         return timestamp;
     }
 
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
+    public double getTotalDistance() {
+        return totalDistance;
     }
 }
--- src/main/java/no/ntnu/idata2900/project/esg_module_backend/sources/FakeDataSource.java
+++ src/main/java/no/ntnu/idata2900/project/esg_module_backend/sources/FakeDataSource.java
@@ -2,9 +2,11 @@ package no.ntnu.idata2900.project.esg_module_backend.sources;
 
 import java.util.Timer;
 import java.util.TimerTask;
-import no.ntnu.idata2900.project.esg_module_backend.data.DummyBoatData;
 import no.ntnu.idata2900.project.esg_module_backend.models.BoatData;
 import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class FakeDataSource implements DataSource {
@@ -12,23 +14,49 @@ public class FakeDataSource implements DataSource {
     private DataListener listener;
     private int i = 0;
 
+    private BoatData fakeBoatData;
+    private ZonedDateTime timestamp;
+
     @Override
     public void start() {
-        DummyBoatData dummyBoatData = new DummyBoatData();
+        fakeBoatData = createInitialBoatData();
+        timestamp = ZonedDateTime.parse("2025-02-13T10:15:30Z");
         timer = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                if (listener != null) {
-                    BoatData data = dummyBoatData.getDummyBoatData(i);
-                    listener.onDataReceived(data);
+                if (i < 50) {
+                    if (listener != null) {
+                        fakeBoatData = generateBoatData();
+                        listener.onDataReceived(fakeBoatData);
+                    }
+                    i = (i + 1);
+                } else {
+                    stop();
                 }
-                i = (i + 1) % 5;
             }
         };
 
         timer.schedule(task, 0, 15000);
         System.out.println("FakeDataSource started");
+    }
+
+    private BoatData createInitialBoatData() {
+        return new BoatData(1, "Boat1", 61.6031484, 5.0445668, 90.0, 85.0, 12.5,
+                "2025-02-13T10:15:30Z", 100.0, 500.0, 0.0);
+    }
+
+    private BoatData generateBoatData() {
+        double lat = fakeBoatData.getLat() + 0.00001;
+        double lng = fakeBoatData.getLng() + 0.00001;
+        timestamp = timestamp.plusMinutes(1);
+        String timestampStr = timestamp.format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        double fishAmount = fakeBoatData.getFishAmount() + 5.0;
+        double fuelLevel = fakeBoatData.getFuelLevel() - 2.0;
+        double totalDistance = fakeBoatData.getTotalDistance() + 5.0;
+
+        return new BoatData(1, "Boat1", lat, lng, 90.0, 85.0, 12.5,
+                timestampStr, fishAmount, fuelLevel, totalDistance);
     }
 
     @Override
