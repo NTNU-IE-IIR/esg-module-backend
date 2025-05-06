367423be0b83775f85fc011544cb0e4aa17a7f24
sinbruh <41494471+sinbruh@users.noreply.github.com>
2025-05-06 15:04:58 +0200

    BAC-177 Create dtos

---
+++ src/main/java/no/ntnu/idata2900/project/esg_module_backend/dtos/DataPointDto.java
@@ -0,0 +1,67 @@
+package no.ntnu.idata2900.project.esg_module_backend.dtos;
+
+import no.ntnu.idata2900.project.esg_module_backend.models.data_points.DataPoint;
+import no.ntnu.idata2900.project.esg_module_backend.models.data_points.Position;
+import no.ntnu.idata2900.project.esg_module_backend.models.data_points.Vessel;
+
+/**
+ * The DataPointDto class represents only the data from a DataPoint that is relevant to send to
+ * the frontend. The class is part of the data packaged into a TripDto.
+ *
+ * @author Group 14
+ * @version v0.2.8 (2025.05.01)
+ */
+public class DataPointDto {
+  private Long id;
+  private long ts;
+  private Position pos;
+  private Vessel vessel;
+
+  /**
+   * Constructor for the DataPoint class, from an existing DataPoint.
+   *
+   * @param dataPoint the specified DataPoint to copy from.
+   */
+  public DataPointDto(DataPoint dataPoint) {
+    this.id = dataPoint.getId();
+    this.ts = dataPoint.getTs();
+    this.pos = dataPoint.getPos();
+    this.vessel = dataPoint.getVessel();
+  }
+
+  /**
+   * Getter for ID.
+   *
+   * @return ID
+   */
+  public Long getId() {
+    return this.id;
+  }
+
+  /**
+   * Getter for timestamp.
+   *
+   * @return Timestamp
+   */
+  public long getTs() {
+    return this.ts;
+  }
+
+  /**
+   * Getter for position data.
+   *
+   * @return Position data
+   */
+  public Position getPos() {
+    return this.pos;
+  }
+
+  /**
+   * Getter for vessel data.
+   *
+   * @return Vessel data
+   */
+  public Vessel getVessel() {
+    return this.vessel;
+  }
+}
