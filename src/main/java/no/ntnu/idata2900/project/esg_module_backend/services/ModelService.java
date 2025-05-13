f7939136f44d04fdd80ab5364f03c225fa8b70d3
Mats Bakketeig <mats.bakketeig@outlook.com>
2025-05-13 23:24:54 +0200

    BAC-202 Add metod for fetching target values and use it in data source

--- src/main/java/no/ntnu/idata2900/project/esg_module_backend/services/ModelService.java
+++ src/main/java/no/ntnu/idata2900/project/esg_module_backend/services/ModelService.java
@@ -3,11 +3,14 @@ package no.ntnu.idata2900.project.esg_module_backend.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -22,12 +25,14 @@ import no.ntnu.idata2900.project.esg_module_backend.repositories.data_points.Dat
  * service.
  * 
  * @author Group 14
- * @version v0.2.0 (2025.05.13)
+ * @version v0.3.0 (2025.05.13)
  */
 @Service
 public class ModelService {
   private DataPointRepository repo;
   private RestTemplate restTemplate;
+
+  private final Logger logger = LoggerFactory.getLogger(ModelService.class);
 
   /**
    * Constructor for the ModelService class. The constructor is annotated with
@@ -43,6 +48,42 @@ public class ModelService {
     this.restTemplate = restTemplateBuilder
         .errorHandler(new RestTemplateResponseErrorHandler())
         .build();
+  }
+
+  /**
+   * Fetches target values for vessel data by making the machine learning model make predictions on
+   * the specified data point. If an error occurs when making predictions, the specified data point
+   * is returned as it was passed, without target values.
+   * 
+   * @param dp The specified data point
+   * @return Data point with target values on success or same data point on error
+   * @see DataPoint
+   */
+  public DataPoint fetchTargetValues(DataPoint dp) {
+    ResponseEntity<String> res = this.predict(dp);
+
+    if (res.getStatusCode() == HttpStatus.OK) {
+      String resBody = res.getBody();
+
+      String[] values = resBody.split(":");
+
+      // Target values as strings
+      String targetSpeedString = values[0];
+      String targetFuelConsumptionString = values[1].substring(1, values[1].length() - 1);
+      // Target values
+      float targetSpeed = Float.parseFloat(targetSpeedString);
+      float targetFuelConsumption = Float.parseFloat(targetFuelConsumptionString);
+
+      dp.getVessel().setTargetSpeed(targetSpeed);
+      dp.getVessel().setTargetFuelConsumption(targetFuelConsumption);
+
+      // Update data point in storage
+      repo.save(dp);
+    } else {
+      logger.error("Could not make prediction on data point");
+    }
+
+    return dp;
   }
 
   /**
