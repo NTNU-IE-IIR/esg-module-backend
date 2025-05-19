053e21caf822a5bdf3e2e15b245c7471a914732d
mathi <mathiaswestergaardjohansen@gmail.com>
2025-05-19 14:54:20 +0200

    removed unused import

--- src/test/java/no/ntnu/idata2900/project/esg_module_backend/integration/TripLogControllerIntegrationTest.java
+++ src/test/java/no/ntnu/idata2900/project/esg_module_backend/integration/TripLogControllerIntegrationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
